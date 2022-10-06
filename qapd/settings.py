105af8587b5f89e337766e0264a8166d1ada07e0
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-10-06 00:58:23 +0000

    Additions

--- qapd/settings.py
+++ qapd/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-1d(m4_=d1@%2j!__3s6tmdpcikar--_=d)&z12#uo@3w5$qdm8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
