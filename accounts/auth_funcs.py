9aa6ae54811fe5600eaf6c004b18e726b7e798d9
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-10-09 22:26:54 +0000

    USE_TZ set to True

--- accounts/auth_funcs.py
+++ accounts/auth_funcs.py
@@ -7,7 +7,7 @@ import pytz
 from django.utils.timezone import make_aware
 
 # Using current time
-ini_time_for_now = datetime.now()
+ini_time_for_now = timezone.now()
 
 future_date_after_7days = ini_time_for_now + \
                           timedelta(days=settings.EMAIL_CONFIRMATION_PERIOD_DAYS)
