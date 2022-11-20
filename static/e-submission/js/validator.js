08626d8a8703ae23e346cb5389c93e26f53b17c2
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-11-20 19:07:21 +0000

    validating form

--- static/e-submission/js/validator.js
+++ static/e-submission/js/validator.js
@@ -4,9 +4,9 @@ const input = document.querySelector('input[type="radio"]');
 const formObj = document.querySelector('form');
 console.log("loaded...", formObj)
 formObj.addEventListener('submit', function(e){
-    console.log("clicked on submit")
+
     if(!input.checked){
-        console.log('Please select all queries')
+        console.log('Please select all queries');
         formObj.prepend(p)
         e.preventDefault()
     }
