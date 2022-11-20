49e4ce78b8dda44387b6e8da90fdd64298891fa4
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-11-20 18:23:26 +0000

    validating form

--- static/e-submission/js/validator.js
+++ static/e-submission/js/validator.js
@@ -1,14 +1,14 @@
 const p = document.createElement('p');
 p.innerText = 'You have some questions unanswered'
 const input = document.querySelector('input[type="radio"]')
-const formObj = document.querySelector('form#formEval')
+const formObj = document.querySelector('form')
 console.log("loaded...")
 formObj.addEventListener('submit', function(e){
+    console.log("clicked on submit")
     if(!input.checked){
         console.log('Please select all queries')
-    formObj.prepend(p)
-    e.preventDefault()
-
+        formObj.prepend(p)
+        e.preventDefault()
     }
 
 })
