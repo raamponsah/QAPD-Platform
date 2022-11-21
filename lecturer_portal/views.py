1d89757b7b64aac9b1faef67fafc8b1a09d67aa0
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-11-21 19:01:36 +0000

    change first() to get()

--- lecturer_portal/views.py
+++ lecturer_portal/views.py
@@ -12,7 +12,7 @@ from helper_functions.helpers import computational_stats, statistics
 
 @only_lecturer
 def lecturer_dashboard(request):
-    lecturer_user = CustomUser.objects.filter(id=request.user.id).first()
+    lecturer_user = CustomUser.objects.filter(id=request.user.id).get()
     lecturer_profile = LecturerProfile.objects.get(user=lecturer_user)
     lecturer_courses = CourseInformation.objects.filter(lecturer_code=lecturer_profile.staff_id)
     number_lecturer_courses = CourseInformation.objects.filter(lecturer_code=lecturer_profile.staff_id).count()
