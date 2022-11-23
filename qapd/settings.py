06e0f61fd3cd9f68386314b301fe634a352648be
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-11-23 12:52:28 +0000

    Debug set false

--- qapd/settings.py
+++ qapd/settings.py
@@ -38,6 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ["https://*.apqad.up.railway.app/", "https://apqad.up.railway.app/"]
 # Application definition
