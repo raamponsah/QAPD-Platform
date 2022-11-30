4ef3453c19bd5012e5b14774d8be1c02c7bd56c2
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-11-30 15:36:29 +0000

    Upgrade of website

---
+++ static/website/main.js
@@ -0,0 +1,25 @@
+const playStudentVideo = document.getElementById('play-student-video');
+const dialog = document.querySelector('#videoDialog');
+const video = document.querySelector("#video")
+const close = document.querySelector("#close")
+
+playStudentVideo.addEventListener('click', () => {
+    if (typeof dialog.showModal === "function") {
+        dialog.showModal();
+        video.play();
+        console.log('showing dialog');
+      } else {
+        console.log("Sorry, the <dialog> API is not supported by this browser.");
+      }
+
+});
+
+close.addEventListener('click', () => {
+    if (typeof dialog.close === "function") {
+        dialog.close();
+        video.pause();
+        console.log('closing dialog');
+        } else {
+        console.log("Sorry, the <dialog> API is not supported by this browser.");
+        }
+});
