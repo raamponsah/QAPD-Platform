872f08292d416dd90454282d7d337cec35ec8cab
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-12-02 12:07:09 +0000

    maintenance mode on

--- accounts/middleware/LecturerProfileMiddleware.py
+++ accounts/middleware/LecturerProfileMiddleware.py
@@ -14,7 +14,7 @@ exception_urls_ = [reverse('admin:login'),
                    reverse('register_student'),
                    reverse('register_lecturer'),
 
-                   reverse('password_reset_confirm'),
+                   # reverse('password_reset_confirm'),
                    ]
 
 
@@ -56,8 +56,8 @@ def utilityfunc(path):
         return redirect('password_reset_done')
     elif path == reverse('password_reset_complete'):
         return redirect('password_reset_complete')
-    elif path == reverse(reset_pattern_url):
-        return redirect(reset_pattern_url)
+    # elif path == reverse(reset_pattern_url):
+    #     return redirect(reset_pattern_url)
     elif path == reverse('admin:login'):
         return redirect('admin:login')
     else:
