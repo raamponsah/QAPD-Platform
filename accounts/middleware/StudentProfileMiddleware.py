6ce4bbb00a0a46bd9a03f6c8192808c2cca0a008
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-12-02 09:47:23 +0000

    groll

--- accounts/middleware/StudentProfileMiddleware.py
+++ accounts/middleware/StudentProfileMiddleware.py
@@ -44,8 +44,8 @@ def utilityfunc(path):
         return redirect('password_reset_done')
     elif path == reverse('password_reset_complete'):
         return redirect('password_reset_complete')
-    elif path == reverse('password_reset_confirm'):
-        return redirect('password_reset_confirm')
+    # elif path == reverse('password_reset_confirm'):
+    #     return redirect('password_reset_confirm')
     elif path == reverse('admin:login'):
         return redirect('admin:login')
     else:
