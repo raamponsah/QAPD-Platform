ce7f453e9de2634dc8257ba35174c9a26e1ae6b2
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-12-04 05:29:57 +0000

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,9 +24,13 @@ urlpatterns = [
 
     # password resets process urls
     path('password/reset', password_reset_request, name='password_reset_request'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"),
+                     name='password_reset_confirm'),
     path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'),
          name='password_reset_done'),
+
+
     # re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
     #         auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"),
     #         name='password_reset_confirm'),
