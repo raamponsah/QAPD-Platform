4aeaedf7a810c970df432cee7203b2060b2931a1
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-12-04 09:10:19 +0000

    middleware update 5

--- accounts/middleware/RouterSafeGuardMiddleware.py
+++ accounts/middleware/RouterSafeGuardMiddleware.py
@@ -12,7 +12,11 @@ class RouterMiddleware:
         # self.whitelisted_urls = '/dashboard/factual/limiting/(?P<token>[^/]+)\\Z'
         self.whitelisted_urls = '/accounts/confirm-email/(?P<token>[^/]+)\\Z'
 
-        self.exception_urls = list([
+
+        self.get_response = get_response
+
+    def __call__(self, request):
+        exception_urls = list([
             reverse('admin:login'),
             reverse('welcome'),
             reverse('password_reset_request'),
@@ -30,17 +34,32 @@ class RouterMiddleware:
                     kwargs={'uidb64': request.GET.get('uidb64', None),
                             'token': request.GET.get('token', None)}),
         ])
-        self.get_response = get_response
-
-    def __call__(self, request):
         response = self.get_response(request)
         if not request.user.is_authenticated:
-            if request.path not in self.exception_urls:
-                return self.utilityfunc(response)
+            if request.path not in exception_urls:
+                return self.utilityfunc(request,response)
         return response
 
-    def utilityfunc(self, rpath):
-        if rpath in self.exception_urls:
+    def utilityfunc(self, request, rpath):
+        exception_urls = list([
+            reverse('admin:login'),
+            reverse('welcome'),
+            reverse('password_reset_request'),
+            reverse('password_reset_done'),
+            reverse('password_reset_complete'),
+            reverse('login_student'),
+            reverse('login_lecturer'),
+            reverse('login_administrator'),
+            reverse('register_student'),
+            reverse('register_lecturer'),
+            reverse('password_reset_request'),
+            reverse('password_reset_done'),
+            reverse('password_reset_complete'),
+            reverse('password_reset_confirm',
+                    kwargs={'uidb64': request.GET.get('uidb64', None),
+                            'token': request.GET.get('token', None)}),
+        ])
+        if rpath in exception_urls:
             return redirect(rpath)
         else:
             return redirect('welcome')
