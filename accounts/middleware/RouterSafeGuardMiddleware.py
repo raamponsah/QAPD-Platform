5788f394f227e86f489dc29441f28704cd229995
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-12-04 19:10:09 +0000

    middleware update 28

--- accounts/middleware/RouterSafeGuardMiddleware.py
+++ accounts/middleware/RouterSafeGuardMiddleware.py
@@ -21,12 +21,13 @@ class RouterMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+
+        response = self.get_response(request)
         if request.path in whitelisted_urls(request) and not request.user.is_authenticated:
             return utilityfunc(request, request.path)
         elif not request.user.is_authenticated:
             return utilityfunc(request, 'welcome')
 
-        response = self.get_response(request)
         return response
 
     # def process_view(self, request, view_func, view_args, view_kwargs):
