dcf2aba2f970b3a8b11509c74eb98fef8b42e80b
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-12-04 07:03:44 +0000

    new updates

--- accounts/middleware/LecturerProfileMiddleware.py
+++ accounts/middleware/LecturerProfileMiddleware.py
@@ -4,28 +4,29 @@ from django.urls import reverse
 
 from accounts.models import LecturerProfile
 
-exception_urls_ = [reverse('admin:login'),
-                   reverse('welcome'),
-                   reverse('password_reset_request'),
-                   reverse('password_reset_done'),
-                   reverse('password_reset_complete'),
-                   reverse('login_student'),
-                   reverse('login_lecturer'),
-                   reverse('login_administrator'),
-                   reverse('register_student'),
-                   reverse('register_lecturer'),
-                   reverse('password_reset_request'),
-                   reverse('password_reset_done'),
-                   reverse('password_reset_complete'),
-                   reverse('password_reset_confirm',
-                           kwargs={'uidb64': request.GET.get('uidb64', None), 'token': request.GET.get('token', None)}),
 
-                   ]
 
 
 def lpm(get_response):
     def middleware(request):
-
+        reset_pattern_url = '/accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
+        exception_urls_ = [reverse('admin:login'),
+                           reverse('welcome'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('login_student'),
+                           reverse('login_lecturer'),
+                           reverse('login_administrator'),
+                           reverse('register_student'),
+                           reverse('register_lecturer'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('password_reset_confirm',
+                                   kwargs={'uidb64': request.GET.get('uidb64', None),
+                                           'token': request.GET.get('token', None)}),
+                           ]
         response = get_response(request)
         try:
             if request.user.id is not None and request.user.is_lecturer is True:
@@ -40,6 +41,24 @@ def lpm(get_response):
 
 def check_authenticated_user(get_response):
     def middleware(request):
+        reset_pattern_url = '/accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
+        exception_urls_ = [reverse('admin:login'),
+                           reverse('welcome'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('login_student'),
+                           reverse('login_lecturer'),
+                           reverse('login_administrator'),
+                           reverse('register_student'),
+                           reverse('register_lecturer'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('password_reset_confirm',
+                                   kwargs={'uidb64': request.GET.get('uidb64', None),
+                                           'token': request.GET.get('token', None)}),
+                           ]
         response = get_response(request)
         if request.user.id is None:
             while request.path not in list(exception_urls_):
--- accounts/middleware/StudentProfileMiddleware.py
+++ accounts/middleware/StudentProfileMiddleware.py
@@ -5,27 +5,27 @@ from django.urls import reverse
 from accounts.middleware.LecturerProfileMiddleware import utilityfunc
 from accounts.models import LecturerProfile, Student
 
-reset_pattern_url = '/accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
-exception_urls_ = [reverse('admin:login'),
-                   reverse('welcome'),
-                   reverse('password_reset_request'),
-                   reverse('password_reset_done'),
-                   reverse('password_reset_complete'),
-                   reverse('login_student'),
-                   reverse('login_lecturer'),
-                   reverse('login_administrator'),
-                   reverse('register_student'),
-                   reverse('register_lecturer'),
-                   reverse('password_reset_request'),
-                   reverse('password_reset_done'),
-                   reverse('password_reset_complete'),
 
-                   reverse('password_reset_confirm',
-                           kwargs={'uidb64': request.GET.get('uidb64', None),
-                                   'token': request.GET.get('token', None)}),
-                   ]
 def spm(get_response):
     def middleware(request):
+        reset_pattern_url = '/accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
+        exception_urls_ = [reverse('admin:login'),
+                           reverse('welcome'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('login_student'),
+                           reverse('login_lecturer'),
+                           reverse('login_administrator'),
+                           reverse('register_student'),
+                           reverse('register_lecturer'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('password_reset_confirm',
+                                   kwargs={'uidb64': request.GET.get('uidb64', None),
+                                           'token': request.GET.get('token', None)}),
+                           ]
 
         response = get_response(request)
         try:
@@ -42,6 +42,24 @@ def spm(get_response):
 
 def check_authenticated_user(get_response):
     def middleware(request):
+        reset_pattern_url = '/accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
+        exception_urls_ = [reverse('admin:login'),
+                           reverse('welcome'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('login_student'),
+                           reverse('login_lecturer'),
+                           reverse('login_administrator'),
+                           reverse('register_student'),
+                           reverse('register_lecturer'),
+                           reverse('password_reset_request'),
+                           reverse('password_reset_done'),
+                           reverse('password_reset_complete'),
+                           reverse('password_reset_confirm',
+                                   kwargs={'uidb64': request.GET.get('uidb64', None),
+                                           'token': request.GET.get('token', None)}),
+                           ]
 
         response = get_response(request)
         if request.user.id is None:
