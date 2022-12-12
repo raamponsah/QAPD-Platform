0c74d923228c720a12fbf57be9686c1e14120c37
Raphael Amponsah <ramponsah026@st.ug.edu.gh>
2022-12-12 11:26:19 +0000

    update on: lecturer and student router

--- accounts/middleware/LecturerProfileMiddleware.py
+++ accounts/middleware/LecturerProfileMiddleware.py
@@ -1,18 +1,18 @@
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse
 
-from accounts.models import LecturerProfile
+from accounts.models import LecturerProfile, CustomUser
 
 
 def lpm(get_response):
     def middleware(request):
         response = get_response(request)
+        lecturer = CustomUser.objects.get(id=request.user.id)
+        print("from lecturer router => ", lecturer)
         try:
-            if request.user.id is not None and request.user.is_lecturer is True:
-                LecturerProfile.objects.filter(user=request.user).get()
+            if lecturer is not None and lecturer.is_lecturer is True and lecturer.is_active is True:
+                return response
         except LecturerProfile.DoesNotExist:
             while not (request.path == reverse('setup-lecturer-profile', kwargs={'user_id': request.user.id})):
                 return redirect(reverse('setup-lecturer-profile', kwargs={'user_id': request.user.id}))
-        return response
-
     return middleware
--- accounts/middleware/StudentProfileMiddleware.py
+++ accounts/middleware/StudentProfileMiddleware.py
@@ -1,19 +1,18 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect
 from django.urls import reverse
 
-from accounts.models import LecturerProfile, Student
+from accounts.models import Student, CustomUser
 
 
 def spm(get_response):
     def middleware(request):
         response = get_response(request)
+        student = CustomUser.objects.get(id=request.user.id)
+        print("from student router => ", student)
         try:
-            if request.user.id is not None and request.user.is_student is True and request.user.is_active is True:
-                Student.objects.filter(user=request.user).get()
-                # return redirect(reverse('login_student'))
+            if student is not None and student.is_student is True and student.is_active is True:
+                return response
         except Student.DoesNotExist:
             while not (request.path == reverse('student_profile_create', kwargs={'pk': request.user.id})):
                 return redirect(reverse('student_profile_create', kwargs={'pk': request.user.id}))
-        return response
-
     return middleware
